bf30e736aa89daafe24ea6b740a918ffd662c917
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-07-13 17:55:02 +0300

    upgrade files

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import styles from './ContactList.module.css';
+
+const ContactList = ({ filterContacts, deleteContact }) => {
+  return (
+    <div className={styles.item}>
+      {filterContacts.map(({ id, number, name }) => {
+        return (
+          <li key={id}>
+            {name}: {number}
+            <div className={styles.position}>
+            <button className={styles.button} onClick={() => deleteContact(id)}>
+              Delete
+              </button>
+              </div>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+
+ContactList.propTypes = {
+  deleteContact: PropTypes.func.isRequired,
+  filterContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default ContactList;
